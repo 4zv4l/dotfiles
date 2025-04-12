17a44d216d031cef3a71806a773318b5af4a3ac7
4zv4l <4zv4l@protonmail.com>
2025-04-12 14:50:40 +0800

    fix: setup script right path for zellij

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fix zellij config
-perl -i.bak -pe 's/\$HOME/$ENV{HOME}/e' ./dotfiles/zellij/config.kdl ./dotfiles/zellij/my-layout.kdl
+perl -i.bak -pe 's/\$HOME/$ENV{HOME}/e' ./dotfiles/.config/zellij/{config.kdl,my-layout.kdl}
 
 # install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
