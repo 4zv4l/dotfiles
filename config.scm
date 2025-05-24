08dc0e5c47ee7de51d9a47bf2274cd42f3f8d8cb
4zv4l <4zv4l@protonmail.com>
2025-05-24 23:35:21 +0800

    up: guix home

--- config.scm
+++ config.scm
@@ -12,76 +12,96 @@
 
 (define %PATH
   (string-replace-substring
-    (string-join
-      (list "$HOME/.local/bin"
-            "$HOME/perl5/bin"
-            "$PATH") ":")
-    "$HOME" (getenv "HOME")))
+   (string-join
+    (list "$HOME/.local/bin"
+          "$HOME/perl5/bin"
+          "$PATH") ":")
+   "$HOME" (getenv "HOME")))
 
 (define %my-shepherd-services
   (list (shepherd-service
-          (documentation "Run Syncthing.")
-          (provision '(syncthing))
-          (start #~(make-forkexec-constructor
-                     '("syncthing" "-no-browser")
-                     #:log-file (string-append (getenv "HOME")
-                                               "/log/syncthing.log")))
-          (stop #~(make-kill-destructor)))))
+         (documentation "Run Syncthing.")
+         (provision '(syncthing))
+         (start #~(make-forkexec-constructor
+                   '("syncthing" "-no-browser")
+                   #:log-file (string-append (getenv "HOME")
+                                             "/log/syncthing.log")))
+         (stop #~(make-kill-destructor)))))
 
 (home-environment
-  (packages
-    (specifications->packages
-      (list "tmux"
-            "zoxide"
-            "fzf"
-            ;;"neovim" version from guix is not yet 10
-            "bat"
-            "lsd"
-            "senpai"
-            "perl"
-            "guile"
-            "guile-lsp-server"
-            "zig"
-            "zig-zls"
-            "gcc-toolchain"
-            "pkg-config"
-            "nushell"
-            "zstd"
-            "pigz"
-            "zip"
-            "unzip"
-            "mtr"
-            "ltrace"
-            "strace"
-            "nmap")))
+ (packages
+  (specifications->packages
+   (list "fortunes-jkirchartz"
+         "cowsay"
+         "lolcat"
+         "tmux"
+         "zoxide"
+         "fzf"
+         ;;"neovim" version from guix is not yet 10
+         "emacs"
+         "bat"
+         "lsd"
+         "senpai"
+         "perl"
+         "guile"
+         "guile-lsp-server"
+         "zig"
+         "zig-zls"
+         "gcc-toolchain"
+         "pkg-config"
+         "nushell"
+         "zstd"
+         "pigz"
+         "zip"
+         "unzip"
+         "mtr"
+         "ltrace"
+         "strace"
+         "nmap")))
 
-  (services
-    (cons*
-      ;; basic bash setup
-      (service home-bash-service-type
-        (home-bash-configuration
-          (environment-variables
-            `(("PATH"           . ,%PATH)
-              ("PS1"            . "$(history -a;history -n)$PS1")
-              ("HISTSIZE"       . "-1")
-              ("HISTFILESIZE"   . "-1")
-              ("EDITOR"         . "nvim")))
-          (aliases
-            '(("cat" . "bat")
-              ("ls"  . "lsd")
-              ("v"   . "nvim")))
-          (bashrc
-            (list (plain-file "rc"
-                    (string-join
-                      (list "eval \"$(zoxide init bash)\""
-                            "eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)")
-                      "\n"))))))
-      ;; copy dotfiles as symlinks
-      (service home-dotfiles-service-type
-        (home-dotfiles-configuration
-        (directories (list "dotfiles"))))
-      ;; shepherd services
-      (service home-shepherd-service-type
-        (home-shepherd-configuration
-          (services %my-shepherd-services)))
-      %base-home-services)))
+ (services
+  (cons*
+   (service home-fish-service-type
+            (home-fish-configuration
+             (environment-variables
+              `(("PATH"           . ,%PATH)             
+                ("EDITOR"         . "nvim")))
+             (aliases
+              '(("cat" . "bat")
+                ("ls"  . "lsd")
+                ("v"   . "nvim")))
+             (config
+              (list (plain-file "rc"
+                                (string-join
+                                 (list "eval (perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
+                                       "zoxide init fish | source"
+                                       "function fish_greeting; echo 'Hello sibl !';end")
+				       ;; "function fish_greeting; fortune | cowsay | lolcat;end")
+                                "\n"))))))
+   (service home-bash-service-type
+	    (home-bash-configuration
+	     (environment-variables
+	      `(("PATH"           . ,%PATH)
+		("PS1"            . "$(history -a;history -n)$PS1")
+		("HISTSIZE"       . "-1")
+		("HISTFILESIZE"   . "-1")
+		("EDITOR"         . "nvim")))
+	     (aliases
+	      '(("cat" . "bat")
+		("ls"  . "lsd")
+		("v"   . "nvim")))
+	     (bashrc
+	      (list (plain-file "rc"
+				(string-join
+				 (list "eval \"$(zoxide init bash)\""
+				       "eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)")
+				 "\n"))))))
+   ;; copy dotfiles as symlinks
+   (service home-dotfiles-service-type
+            (home-dotfiles-configuration
+             (directories (list "dotfiles"))))
+   ;; shepherd services
+   (service home-shepherd-service-type
+            (home-shepherd-configuration
+             (services %my-shepherd-services)))
+   %base-home-services)))
